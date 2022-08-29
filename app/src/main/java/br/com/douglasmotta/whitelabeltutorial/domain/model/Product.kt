67ec6c2153390cd0f2df04baf9f04ece5af94490
João Pedro <joaoplay16@gmail.com>
2022-08-29 20:36:37 -0300

    changes in the class Product

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/domain/model/Product.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/domain/model/Product.kt
@@ -1,8 +1,15 @@
 package br.com.douglasmotta.whitelabeltutorial.domain.model
 
+import android.os.Parcelable
+import com.google.firebase.firestore.PropertyName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Product(
     val id: String = "",
     val description: String = "",
     val priceUrl: Double = 0.0,
-    val imageUrl: String = ""
-)
+    @get:PropertyName("image_url")
+    @set:PropertyName("image_url")
+    var imageUrl: String = ""
+): Parcelable
