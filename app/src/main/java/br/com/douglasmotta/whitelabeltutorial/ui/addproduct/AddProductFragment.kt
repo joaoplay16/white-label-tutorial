77c95fce5e734012cbc6d3cb087b7a680ead6a2a
João Pedro <joaoplay16@gmail.com>
2022-08-30 20:30:03 -0300

    AddProductFragment inherits BottomSheetDialogFragment

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/addproduct/AddProductFragment.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/addproduct/AddProductFragment.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import br.com.douglasmotta.whitelabeltutorial.databinding.FragmentAddProductBinding
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
-class AddProductFragment : Fragment() {
+class AddProductFragment : BottomSheetDialogFragment() {
 
     private var _binding: FragmentAddProductBinding? = null
     private val binding get() = _binding!!
