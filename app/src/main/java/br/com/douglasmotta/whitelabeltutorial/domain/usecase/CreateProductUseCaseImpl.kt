e650ac6b355787afe6e487bb1cfef0221075d0a1
João Pedro <joaoplay16@gmail.com>
2022-08-30 19:52:31 -0300

    class CreateProductUseCaseImpl created

---
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/domain/usecase/CreateProductUseCaseImpl.kt
@@ -0,0 +1,27 @@
+package br.com.douglasmotta.whitelabeltutorial.domain.usecase
+
+import android.net.Uri
+import br.com.douglasmotta.whitelabeltutorial.data.ProductRepository
+import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
+import java.util.*
+
+class CreateProductUseCaseImpl(
+    private val uploadProductImageUseCase: UploadProductImageUseCase,
+    private val productRepository: ProductRepository
+): CreateProductUseCase{
+
+    override suspend operator fun invoke(
+        description: String,
+        price: Double,
+        imageUri: Uri
+    ): Product{
+        return try {
+            val imageUrl = uploadProductImageUseCase(imageUri)
+            val product = Product(UUID.randomUUID().toString(), description, price, imageUrl)
+            productRepository.createProduct(product)
+        }catch (e: Exception){
+            throw e
+        }
+    }
+
+}
