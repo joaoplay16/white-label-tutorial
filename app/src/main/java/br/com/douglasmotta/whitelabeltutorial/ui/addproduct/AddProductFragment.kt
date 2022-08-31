f892bb4b2d55321a659a1697291954df6d5b4adc
João Pedro <joaoplay16@gmail.com>
2022-08-31 19:37:02 -0300

    implementing views listeners

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/addproduct/AddProductFragment.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/addproduct/AddProductFragment.kt
@@ -1,16 +1,28 @@
 package br.com.douglasmotta.whitelabeltutorial.ui.addproduct
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
 import br.com.douglasmotta.whitelabeltutorial.databinding.FragmentAddProductBinding
+import br.com.douglasmotta.whitelabeltutorial.util.CurrencyTextWatcher
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class AddProductFragment : BottomSheetDialogFragment() {
 
     private var _binding: FragmentAddProductBinding? = null
     private val binding get() = _binding!!
+
+    private var imageUri: Uri? = null
+
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.GetContent()){ uri ->
+            imageUri = uri
+            binding.imageProduct.setImageURI(uri)
+        }
+
     private lateinit var viewModel: AddProductViewModel
 
     override fun onCreateView(
@@ -21,4 +33,28 @@ class AddProductFragment : BottomSheetDialogFragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setListeners()
+    }
+
+    private fun setListeners(){
+        binding.imageProduct.setOnClickListener {
+            chooseImage()
+        }
+
+        binding.buttonAddProduct.setOnClickListener {
+            val description = binding.inputDescription.text.toString()
+            val price = binding.inputPrice.text.toString()
+        }
+
+        binding.inputPrice.run {
+            addTextChangedListener(CurrencyTextWatcher(this))
+        }
+    }
+
+    private fun chooseImage(){
+        getContent.launch("image/*")
+    }
 }
