ca48e4bf6f9670b95f8034a6f6efae857b4dd04b
João Pedro <joaoplay16@gmail.com>
2022-09-01 18:53:15 -0300

    injected constructors

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/domain/usecase/CreateProductUseCaseImpl.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/domain/usecase/CreateProductUseCaseImpl.kt
@@ -4,8 +4,9 @@ import android.net.Uri
 import br.com.douglasmotta.whitelabeltutorial.data.ProductRepository
 import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
 import java.util.*
+import javax.inject.Inject
 
-class CreateProductUseCaseImpl(
+class CreateProductUseCaseImpl @Inject constructor(
     private val uploadProductImageUseCase: UploadProductImageUseCase,
     private val productRepository: ProductRepository
 ): CreateProductUseCase{
