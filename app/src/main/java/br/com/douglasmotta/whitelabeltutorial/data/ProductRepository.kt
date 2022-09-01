72ab5672a018cec6811f038f611ca35ab4600e53
João Pedro <joaoplay16@gmail.com>
2022-09-01 19:06:21 -0300

    injected constructors

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/ProductRepository.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/data/ProductRepository.kt
@@ -4,7 +4,9 @@ import android.net.Uri
 import br.com.douglasmotta.whitelabeltutorial.domain.model.Product
 import javax.inject.Inject
 
-class ProductRepository @Inject constructor(private val dataSource: ProductDataSource) {
+class ProductRepository @Inject constructor(
+    private val dataSource: ProductDataSource
+) {
 
     suspend fun getProducts(): List<Product> = dataSource.getProducts()
 
