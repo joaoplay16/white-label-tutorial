a94552aa59c9c6d31d779643d600f34eb826eecc
João Pedro <joaoplay16@gmail.com>
2022-09-03 18:41:29 -0300

    using viewModel

--- app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/ProductsFragment.kt
+++ app/src/main/java/br/com/douglasmotta/whitelabeltutorial/ui/products/ProductsFragment.kt
@@ -5,12 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import br.com.douglasmotta.whitelabeltutorial.databinding.FragmentProductsBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class ProductsFragment : Fragment() {
 
     private var _binding: FragmentProductsBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: ProductsViewModel by viewModels()
 
     private val productsAdapter = ProductsAdapter()
 
@@ -26,12 +31,21 @@ class ProductsFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setRecyclerView()
+        observeVMEvents()
+
+        viewModel.getProducts()
     }
 
     private fun setRecyclerView(){
         binding.recyclerProducts.run {
             setHasFixedSize(true)
             adapter = productsAdapter
+        }
+    }
+
+    private fun observeVMEvents(){
+        viewModel.productsData.observe(viewLifecycleOwner) { products ->
+            productsAdapter.submitList(products)
         }
     }
 
